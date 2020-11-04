44f9bf48eca65a57131b86af5586c510e9b7c4e4
Isaac King <idrkings@gmail.com>
2020-11-04 18:14:59 +0000

    additional exercises

---
+++ area-calc.js
@@ -0,0 +1,38 @@
+const calculateRectangleArea = function (length, width) {
+  // if either of the inputs is negative, returns undefined
+  if (length < 0 || width < 0) {
+    return undefined;
+  }
+
+  return length * width;
+}
+
+const calculateTriangleArea = function (base, height) {
+  // if either of the inputs is negative, returns undefined
+  if (base < 0 || height < 0) {
+    return undefined;
+  }
+
+  return base * height / 2;
+}
+
+const calculateCircleArea = function (radius) {
+  // if input is negative, returns undefined
+  if (radius < 0) {
+    return undefined;
+  }
+
+  return Math.PI * (Math.pow(radius, 2));
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
